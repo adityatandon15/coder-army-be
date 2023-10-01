75bebe7c5ef27dc223df9d84b6bce94f77fc8cc0
sachinverma001 <sachinverma@cofactory.ai>
2023-10-01 23:31:16 +0530

    route removed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.get("/", userController.getAllUsers);
+// router.get("/", userController.getAllUsers);
 router.post("/create", userController.createUser);
 router.post("/signin", userController.getUser);
 
